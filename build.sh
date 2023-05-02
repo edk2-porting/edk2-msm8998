834ba5b640cf21417ef7f2fda44100a16ec4e5e0
qaz6750 <2838632319@qq.com>
2023-05-02 19:51:11 +0800

    Project: Refactor: Move edk2 and edk2 platforms to Common
    Signed-off-by: qaz6750 <2838632319@qq.com>

--- build.sh
+++ build.sh
@@ -123,7 +123,7 @@ done
 if "${DISTCLEAN}";then _distclean;exit "$?";fi
 if "${CLEAN}";then _clean;exit "$?";fi
 [ -z "${DEVICE}" ]&&_help 1
-if ! [ -f edk2/edksetup.sh ] || ! [ -f ../edk2/edksetup.sh ]
+if ! [ -f Common/edk2/edksetup.sh ] || ! [ -f ../edk2/edksetup.sh ]
 then	set -e
 	echo "Updating submodules"
 	if "${CHINESE}"
@@ -131,7 +131,7 @@ then	set -e
 		git submodule set-url edk2-platforms https://hub.fastgit.xyz/tianocore/edk2-platforms.git
 		git submodule set-url MSM8998Pkg/Library/SimpleInit https://hub.fastgit.xyz/BigfootACA/simple-init.git
 		git submodule init;git submodule update --depth 1
-		pushd edk2
+		pushd Common/edk2
 
 		git submodule set-url ArmPkg/Library/ArmSoftFloatLib/berkeley-softfloat-3   https://hub.fastgit.xyz/ucb-bar/berkeley-softfloat-3.git
 		git submodule set-url CryptoPkg/Library/OpensslLib/openssl                  https://hub.fastgit.xyz/openssl/openssl.git
@@ -150,7 +150,7 @@ then	set -e
 		popd
 		git checkout .gitmodules
 	else	git submodule init;git submodule update --depth 1
-		pushd edk2
+		pushd Common/edk2
 		git submodule init;git submodule update
 		popd
 		pushd MSM8998Pkg/Library/SimpleInit
@@ -159,13 +159,13 @@ then	set -e
 	fi
 	set +e
 fi
-for i in "${EDK2}" ./edk2 ../edk2
+for i in "${EDK2}" ./Common/edk2 ../edk2
 do	if [ -n "${i}" ]&&[ -f "${i}/edksetup.sh" ]
 	then	_EDK2="$(realpath "${i}")"
 		break
 	fi
 done
-for i in "${EDK2_PLATFORMS}" ./edk2-platforms ../edk2-platforms
+for i in "${EDK2_PLATFORMS}" ./Common/edk2-platforms ../edk2-platforms
 do	if [ -n "${i}" ]&&[ -d "${i}/Platform" ]
 	then	_EDK2_PLATFORMS="$(realpath "${i}")"
 		break
